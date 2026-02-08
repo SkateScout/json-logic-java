2a5dae55cbb0bb4e3ed234d9046b9f5d5e65a717
Thomas Lußnig <thomas@suche.org>
2026-02-08 15:16:05 +0100

    Mavan + Ant build

--- src/main/java/io/github/jamsesso/jsonlogic/NullableDeque.java
+++ src/main/java/io/github/jamsesso/jsonlogic/NullableDeque.java
@@ -2,9 +2,18 @@ package io.github.jamsesso.jsonlogic;
 
 import java.util.ArrayDeque;
 
-public final class NullableDeque<T> extends ArrayDeque<T>{
+public final class NullableDeque<T> extends ArrayDeque<Object>{
+	private static final long serialVersionUID = 1L;
 	private static final Object NULL = new Object();
 	public NullableDeque(final int numElements) { super(numElements); }
-	@Override public void push(final T e) { super.push(null==e?(T)NULL:e); }
-	@Override public T    pop() { final Object r = super.pop(); return(NULL==r?null:(T)r); }
+
+	@Override public void push(final Object e) {
+		super.push(e == null ? NULL : e);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override public T    pop() {
+		final var r = super.pop();
+		return (r == NULL ? null : (T)r);
+	}
 }
