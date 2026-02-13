9091ca9238525d2f2937d9133c8d67c04893b8a7
Thomas Lußnig <thomas@suche.org>
2026-02-13 19:40:22 +0100

    Last error cleanup

--- src/test/java/io/github/jamsesso/jsonlogic/TruthyTests.java
+++ src/test/java/io/github/jamsesso/jsonlogic/TruthyTests.java
@@ -1,46 +1,48 @@
 package io.github.jamsesso.jsonlogic;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.jamsesso.jsonlogic.ast.JSON;
+
 public class TruthyTests {
-  @Test
-  public void testTruthyValues() {
-    // Zero
-    assertFalse(JsonLogic.truthy(0));
+	@Test
+	public void testTruthyValues() {
+		// Zero
+		assertFalse(JSON.truthy(0));
 
-    // Any non-zero number
-    assertTrue(JsonLogic.truthy(1.04));
-    assertTrue(JsonLogic.truthy(-1));
+		// Any non-zero number
+		assertTrue(JSON.truthy(1.04));
+		assertTrue(JSON.truthy(-1));
 
-    // Empty array or collection
-    assertFalse(JsonLogic.truthy(Collections.EMPTY_LIST));
-    assertFalse(JsonLogic.truthy(new int[0]));
+		// Empty array or collection
+		assertFalse(JSON.truthy(Collections.EMPTY_LIST));
+		assertFalse(JSON.truthy(new int[0]));
 
-    // Any non-empty array or collection
-    assertTrue(JsonLogic.truthy(Collections.singleton(1)));
-    assertTrue(JsonLogic.truthy(new boolean[] {false}));
+		// Any non-empty array or collection
+		assertTrue(JSON.truthy(Collections.singleton(1)));
+		assertTrue(JSON.truthy(new boolean[] {false}));
 
-    // Empty string
-    assertFalse(JsonLogic.truthy(""));
+		// Empty string
+		assertFalse(JSON.truthy(""));
 
-    // Any non-empty string
-    assertTrue(JsonLogic.truthy("hello world"));
-    assertTrue(JsonLogic.truthy("0"));
+		// Any non-empty string
+		assertTrue(JSON.truthy("hello world"));
+		assertTrue(JSON.truthy("0"));
 
-    // Null
-    assertFalse(JsonLogic.truthy(null));
+		// Null
+		assertFalse(JSON.truthy(null));
 
-    // NaN and Infinity
-    assertFalse(JsonLogic.truthy(Double.NaN));
-    assertFalse(JsonLogic.truthy(Float.NaN));
-    assertTrue(JsonLogic.truthy(Double.POSITIVE_INFINITY));
-    assertTrue(JsonLogic.truthy(Double.NEGATIVE_INFINITY));
-    assertTrue(JsonLogic.truthy(Float.POSITIVE_INFINITY));
-    assertTrue(JsonLogic.truthy(Float.NEGATIVE_INFINITY));
-  }
+		// NaN and Infinity
+		assertFalse(JSON.truthy(Double.NaN));
+		assertFalse(JSON.truthy(Float.NaN));
+		assertTrue(JSON.truthy(Double.POSITIVE_INFINITY));
+		assertTrue(JSON.truthy(Double.NEGATIVE_INFINITY));
+		assertTrue(JSON.truthy(Float.POSITIVE_INFINITY));
+		assertTrue(JSON.truthy(Float.NEGATIVE_INFINITY));
+	}
 }
